18beb61fa00d2c8069c6179832fba303b9e896c4
Abdulrahman Ismail <abdulrahmanismail987@gmail.com>
2024-01-14 21:18:05 +0300

    first commit

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -2,7 +2,7 @@ import Hero from "../Component/Hero/Hero";
 import NewCollection from "../Component/NewCollection/NewCollection";
 import NewsLetter from "../Component/NewsLetter/NewsLetter";
 import Offer from "../Component/Offer/Offer";
-import Popular from "../Component/Popular/popular";
+import Popular from "../Component/Popular/Popular";
 
 const Shop = () => {
   return (
